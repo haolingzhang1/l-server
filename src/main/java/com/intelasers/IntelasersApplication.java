9222e8a64aa41130428a80ac425123bc718c5b7e
haolingzhang1 <haolingzhang@outlook.com>
2021-01-22 10:28:46 +0800

    initialize

---
+++ src/main/java/com/intelasers/IntelasersApplication.java
@@ -0,0 +1,13 @@
+package com.intelasers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelasersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelasersApplication.class, args);
+	}
+
+}
